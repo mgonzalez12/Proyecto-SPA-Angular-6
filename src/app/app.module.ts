34b81b00861b47a86dbe4ab843916edf4d57f198
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2018-07-26 12:27:01 -0500

    Router Angular 6 and Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+// componetes
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
