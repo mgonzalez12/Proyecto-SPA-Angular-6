82932645399686741aca8dc0ea12cab9e0a7d6ab
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2018-07-26 20:17:55 -0500

    Router, RouterLink y buscador Angular 6 and Bootstrap

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: []
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:any = {};
 
-  ngOnInit() {
-  }
+  constructor( private activatedRoute: ActivatedRoute,private _heroesService: HeroesService) 
+    {
+    this.activatedRoute.params.subscribe(params => {
+      this.heroe = this._heroesService.getHeroe( params['id']);
+    })
+   }
+  
 
 }
